a359a009ce1b4426e2056515e2628eb2e7c22d09
DiamondHawk8 <87952194+DiamondHawk8@users.noreply.github.com>
2025-06-13 16:27:04 -0400

    Add main logging

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import logging
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import Qt, QEvent
 
-
 from ui.custom_grips import CustomGrip
 from ui.ui_main import Ui_MainWindow
 
@@ -20,9 +19,11 @@ from controllers.import_controller import ImportController
 from controllers.search_controller import SearchController
 from controllers.tab_controller import TabController
 
-
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='logs/oculus.log', level=logging.DEBUG)
 
 ACTIVE_BACKEND = "sqlite"
+
 GALLERY_PAGE_INDEX = 0
 WIDGET_PAGE_INDEX = 1
 IMPORT_PAGE_INDEX = 2
@@ -33,32 +34,17 @@ class MainWindow(QMainWindow):
 
     def __init__(self) -> None:
         super().__init__()
-        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowSystemMenuHint)
-        self.setAttribute(Qt.WA_TranslucentBackground, False)
-        # Make borderless
 
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-
-        # Create grips for window
-        self._grips = [
-            CustomGrip(self, Qt.LeftEdge, disable_color=True),
-            CustomGrip(self, Qt.RightEdge, disable_color=True),
-            CustomGrip(self, Qt.TopEdge, disable_color=True),
-            CustomGrip(self, Qt.BottomEdge, disable_color=True),
-        ]
-
-        # Enable mouse tracking + install event filter
-        self._drag_pos = None
-        self.ui.contentTopBg.setMouseTracking(True)
-        self.ui.contentTopBg.installEventFilter(self)
+        self.setup_window()
 
         self.conn = get_db_connection(db_path="oculus.db", backend=ACTIVE_BACKEND)
+        logger.info("Connected to database")
 
         # backend managers
         self.media = MediaManager(self.conn, parent=self)
         self.tags = TagManager(self.conn)
         self.search = SearchManager(self.conn, self.tags)
+        logger.debug("Main managers instantiated")
 
         # Other Managers
         self.keybinds = KeybindManager(self)
@@ -66,10 +52,12 @@ class MainWindow(QMainWindow):
         # Logic Controllers
 
         self.tab_controller = TabController(tab_widget=self.ui.galleryTabs, keybinds=self.keybinds)
-        self.gallery_controller = GalleryController(self.ui, self.media, self.tags, self.tab_controller, self.ui.gallery_page)
+        self.gallery_controller = GalleryController(self.ui, self.media, self.tags, self.tab_controller,
+                                                    self.ui.gallery_page)
 
         self.search_controller = SearchController(self.ui, self.media, self.search)
         self.import_controller = ImportController(self, self.ui, self.media, self.tags, self.gallery_controller)
+        logger.debug("Controllers instantiated")
 
         # Connect window buttons
         self.ui.closeAppBtn.clicked.connect(self.close)
@@ -94,8 +82,32 @@ class MainWindow(QMainWindow):
         self.ui.btn_adv.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(WIDGET_PAGE_INDEX))
         self.ui.btn_import.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(IMPORT_PAGE_INDEX))
         self.ui.btn_search.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(SEARCH_PAGE_INDEX))
+        logger.info("Main window setup complete")
 
-    # Glue edges
+    def setup_window(self) -> None:
+
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowSystemMenuHint)
+        self.setAttribute(Qt.WA_TranslucentBackground, False)
+
+        # Make borderless
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Create grips for window
+        self._grips = [
+            CustomGrip(self, Qt.LeftEdge, disable_color=True),
+            CustomGrip(self, Qt.RightEdge, disable_color=True),
+            CustomGrip(self, Qt.TopEdge, disable_color=True),
+            CustomGrip(self, Qt.BottomEdge, disable_color=True),
+        ]
+
+        # Enable mouse tracking + install event filter
+        self._drag_pos = None
+        self.ui.contentTopBg.setMouseTracking(True)
+        self.ui.contentTopBg.installEventFilter(self)
+
+        logger.info("Main window setup")
+
     def resizeEvent(self, event):
         super().resizeEvent(event)
         g = 10
@@ -106,7 +118,7 @@ class MainWindow(QMainWindow):
         self._grips[2].setGeometry(0, 0, w, g)
         self._grips[3].setGeometry(0, h - g, w, g)
 
-    # Temp dragging behavior method
+
     def eventFilter(self, obj, event):
         if obj == self.ui.contentTopBg:
             if event.type() == QEvent.Type.MouseButtonPress and event.button() == Qt.LeftButton:
